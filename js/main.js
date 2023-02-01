0e00d2ceb7d124235d24eb21414dc1b69cacc8a6
JackMwisa <mukewajack@gmail.com>
2023-02-01 19:16:35 +0300

    fin

--- js/main.js
+++ js/main.js
@@ -12,18 +12,32 @@ btns.forEach(function (btn) {
         styles = e.currentTarget.classList;
 
 
-        if (styles.contains("decrease")){
+        if (styles.contains("decrease")) {
             count--;
-            
-        } 
-        
-        else if (styles.contains("increase")){
+
+        }
+
+        else if (styles.contains("increase")) {
             count++;
         }
 
         else {
             count = 0;
         }
+
+        if (count > 0) {
+            value.style.color = "green";
+
+        }
+
+        if (count < 0) {
+            value.style.color = "red";
+        }
+
+        if (count === 0) {
+            value.style.color = "black";
+        }
+
 
         value.textContent = count;
 
