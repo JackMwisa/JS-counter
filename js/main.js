2b03b27f13bed8a58bc0da6c3e642f1d3b4fec9f
JackMwisa <mukewajack@gmail.com>
2023-02-01 18:54:49 +0300

    click
    btns.forEach(function (btn) {
        btn.addEventListener("click", function (e) {
            styles = e.currentTarget.classList;
    
        })
    
    });

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// set initail count
+
+let count = 0;
+
+// select values and buttons
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        styles = e.currentTarget.classList;
+
+    })
+
+});
